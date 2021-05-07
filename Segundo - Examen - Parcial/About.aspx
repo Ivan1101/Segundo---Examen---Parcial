fda94e8f10041ee02e8517b4c1d8620a792988ee
Daniel Pérez Chan <danielpc@mesoamericana.edu.gt>
2021-05-07 10:12:37 -0600

    Se realizo el boton de eliminar una canción de un album

--- Segundo - Examen - Parcial/About.aspx
+++ Segundo - Examen - Parcial/About.aspx
@@ -1,7 +1,86 @@
 ﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="Segundo___Examen___Parcial.About" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-    <h2><%: Title %>.</h2>
-    <h3>Your application description page.</h3>
-    <p>Use this area to provide additional information.</p>
+    <div class="jumbotron">
+      <h1> Control de Datos </h1>
+    <br />
+   
+    </div>
+
+        <div class="row">
+                    <div class="col-xs-4" style="left: 0px; top: 0px">
+                                <strong>Dato de:</strong>
+                                <asp:DropDownList ID="DropDownList_mostrar" runat="server" Height="20px" Width="109px" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" AutoPostBack="True">
+                                    <asp:ListItem></asp:ListItem>
+                         <asp:ListItem>Alumno</asp:ListItem>
+                         <asp:ListItem>Catedrático</asp:ListItem>
+                         <asp:ListItem>Personal Administrativo</asp:ListItem>
+                     </asp:DropDownList>
+                         <br />
+                                <asp:Label ID="Label_noexiste" runat="server"></asp:Label>
+                     </div>
+                        <div class="col-xs-3">
+                            <div class="row">
+                            &nbsp; 
+                        </div>
+                              <div class="row">
+                        <asp:Label ID="Label2" runat="server" Text="Ingrese el nombre del artista"></asp:Label>
+       &nbsp;<asp:TextBox ID="TextBox_codigo" runat="server" Height="20px"></asp:TextBox>
+       <br />  
+                        </div>
+                            <div class="row">
+                            <asp:Button ID="Button1" runat="server" CssClass="btn btn-primary btn-sm" Height="31px" OnClick="Button1_Click" Text="Buscar" Width="106px" />
+       <br />
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; 
+       &nbsp;&nbsp; 
+                            </div></div>
+            <div class="col-xs-3">
+                <asp:GridView ID="GridView_Universidades" runat="server" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="3" OnSelectedIndexChanged="GridView_Universidades_SelectedIndexChanged">
+                    <Columns>
+                        <asp:CommandField ShowSelectButton="True" />
+                    </Columns>
+                    <FooterStyle BackColor="White" ForeColor="#000066" />
+                    <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+                    <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+                    <RowStyle ForeColor="#000066" />
+                    <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+                    <SortedAscendingCellStyle BackColor="#F1F1F1" />
+                    <SortedAscendingHeaderStyle BackColor="#007DBB" />
+                    <SortedDescendingCellStyle BackColor="#CAC9C9" />
+                    <SortedDescendingHeaderStyle BackColor="#00547E" />
+                </asp:GridView>
+                </div>
+                    <br />
+                    <br />
+             </div>
+     <br />
+  <div class="row"> <!-- Código para mostrar los datos de la persona a modificar -->
+       <div class="col-xs-3" style="left: 0px; top: 0px">
+                                <asp:Label ID="Label6" runat="server" ForeColor="Black" Text="Nombre:"></asp:Label>
+&nbsp;<asp:TextBox ID="TextBoxNombre" runat="server"></asp:TextBox>
+                            </div>
+                        <div class="col-xs-3">
+                            <asp:Label ID="Label9" runat="server" Text="Artista"></asp:Label>
+&nbsp;<asp:TextBox ID="TextBoxartista" runat="server"></asp:TextBox>
+                        </div>
+                        <div class="col-xs-3">
+                            <asp:Label ID="Label8" runat="server" ForeColor="Black" Text="Duración:"></asp:Label>
+&nbsp;<asp:TextBox ID="TextBoxtiempo" runat="server"></asp:TextBox>
+                        </div>
+              <div class="col-xs-3">
+&nbsp;</div> 
+            </div>
+        <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <br />
+        
+
+    <br />
+    <asp:Button ID="Button2_editar" runat="server" Height="32px" OnClick="Button2_Click" Text="Editar" Width="118px" CssClass="btn btn-warning btn-sm" />
+     <asp:Button ID="Button3_eliminar" runat="server" Height="32px" OnClick="Button4_Click" Text="Eliminar" Width="118px" CssClass="btn btn-danger btn-sm" />
+    <br />
+    <asp:Button ID="Button3_modificar" runat="server" Height="32px" OnClick="Button3_Click" Text="Modificar" Width="118px" CssClass="btn btn-primary btn-sm" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; 
+       
 </asp:Content>
--- Segundo - Examen - Parcial/About.aspx.cs
+++ Segundo - Examen - Parcial/About.aspx.cs
@@ -1,17 +1,180 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Segundo___Examen___Parcial.Clases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
 namespace Segundo___Examen___Parcial
 {
     public partial class About : Page
     {
+        static bool seleccionar; // variable booleanda para mostrar el DropDownList_mostrar
+        static int seleccionada; // variable para ver  la Universidad seleccionada
+        static int seleccionado; // Varriable para seleccionar la persona en el DropDownList
+        static List<Albúm> albums = new List<Albúm>();
+        static List<Canción> canciones = new List<Canción>();
         protected void Page_Load(object sender, EventArgs e)
         {
+            desbloqueo1(false);
+        }
+        public void desbloqueo1(bool ingreso )
+        {
+            TextBox_codigo.Visible = ingreso; TextBox_codigo.Enabled = ingreso;
+             Label2.Visible = ingreso;
+        }
+        public void desbloqueo_mostrar_datos()
+        {
+            Label6.Visible = true; Label8.Visible = true; Label9.Visible = true;
+            TextBoxNombre.Visible = true; TextBoxartista.Visible = true; TextBoxartista.Visible = true;
+            TextBoxtiempo.Visible = true; 
+        }
+        public void desbloqueo_mostrar_editar()
+        {
+            TextBoxNombre.Enabled = true; TextBoxartista.Enabled = true; TextBoxartista.Enabled = true;
+            TextBoxtiempo.Enabled = true; 
+        }
+        public void limpiar_campos()
+        {
+            TextBoxNombre.Text = " ";
+            TextBoxartista.Text = " ";
+            TextBoxtiempo.Text = " ";
+        }
+        public void bloqueo()
+        {
+            TextBoxNombre.Visible = false;
+            TextBoxNombre.Enabled = false;
+            TextBoxartista.Enabled = false; TextBoxartista.Visible = false;
+            Label9.Visible = false;
+            Label8.Visible = false; TextBoxtiempo.Enabled = false;
+            TextBoxtiempo.Visible = false; Label6.Visible = false; Button1.Visible = false;
+        }
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            seleccionado = DropDownList_mostrar.SelectedIndex;
+            if (seleccionado == 1)
+            {
+                desbloqueo1(true); Button1.Visible = true;
+            }
+            else if (seleccionado == 2)
+            {
+                desbloqueo1(true); Button1.Visible = true;
+            }
+            else if (seleccionado == 3)
+            {
+                desbloqueo1(true); Button1.Visible = true;
+            }
+        }
+        public void GuardarJson()
+        {
+            string json = JsonConvert.SerializeObject(albums);
+            string archivo = Server.MapPath("Archivo Albumes.json");
+            System.IO.File.WriteAllText(archivo, json);
+        }
 
+        protected void GridView_Universidades_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DropDownList_mostrar.Visible = true;
+            seleccionada = GridView_Universidades.SelectedIndex;
+            seleccionar = true;
+            canciones = albums[seleccionada].Canciones.ToList();
+            limpiar_campos(); bloqueo(); DropDownList_mostrar.SelectedIndex = 0;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (seleccionado == 1)
+            {
+                Canción search_alumno = new Canción();
+                search_alumno = albums[seleccionada].Canciones.Find(bus => bus.Artista_canción == TextBox_codigo.Text);
+                if (search_alumno == null)
+                {
+                    Label_noexiste.Text = "NO EXISTE EL artista";
+                    desbloqueo1(true); Button1.Visible = true;
+                }
+                else
+                {
+                    Label_noexiste.Text = " ";
+                    TextBoxNombre.Text = search_alumno.Nombre_canción;
+                    TextBoxartista.Text = search_alumno.Artista_canción;
+                    TextBoxtiempo.Text = Convert.ToString(search_alumno.Duración_min);
+                    
+                    Button2_editar.Visible = true; Button3_eliminar.Visible = true;
+                }
+            }
+            else if (seleccionado == 2)
+            {
+                Canción search_alumno = new Canción();
+                search_alumno = albums[seleccionada].Canciones.Find(bus => bus.Artista_canción == TextBox_codigo.Text);
+                if (search_alumno == null)
+                {
+                
+                    Label_noexiste.Text = "NO EXISTE EL artista";
+                    desbloqueo1(true); Button1.Visible = true;
+                }
+                else
+                {
+                    Label_noexiste.Text = " ";
+                    TextBoxNombre.Text = search_alumno.Nombre_canción;
+                    TextBoxartista.Text = search_alumno.Artista_canción;
+                    TextBoxtiempo.Text = Convert.ToString(search_alumno.Duración_min);
+
+                    Button2_editar.Visible = true; Button3_eliminar.Visible = true;
+                }
+            }
+            else if (seleccionado == 3)
+            {
+                Canción search_alumno = new Canción();
+                search_alumno = albums[seleccionada].Canciones.Find(bus => bus.Artista_canción == TextBox_codigo.Text);
+                if (search_alumno == null)
+                {
+                    Label_noexiste.Text = "NO EXISTE EL artista";
+                    desbloqueo1(true); Button1.Visible = true;
+                }
+                else
+                {
+                    Label_noexiste.Text = " ";
+                    TextBoxNombre.Text = search_alumno.Nombre_canción;
+                    TextBoxartista.Text = search_alumno.Artista_canción;
+                    TextBoxtiempo.Text = Convert.ToString(search_alumno.Duración_min);
+
+                    Button2_editar.Visible = true; Button3_eliminar.Visible = true;
+                }
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            if (seleccionado == 1)
+            {
+                int selec_alumno;
+                selec_alumno = albums[seleccionada].Canciones.FindIndex(bus => bus.Artista_canción == TextBox_codigo.Text);
+                albums[seleccionada].Canciones.RemoveAt(selec_alumno);
+                GuardarJson(); limpiar_campos();
+                TextBox_codigo.Text = ""; bloqueo(); desbloqueo1(true); Button1.Visible = true;
+            }
+            else if (seleccionado == 2)
+            {
+                int selec_alumno;
+                selec_alumno = albums[seleccionada].Canciones.FindIndex(bus => bus.Artista_canción == TextBox_codigo.Text);
+                albums[seleccionada].Canciones.RemoveAt(selec_alumno);
+                GuardarJson(); limpiar_campos();
+                TextBox_codigo.Text = ""; bloqueo(); desbloqueo1(true); Button1.Visible = true;
+            }
+            else if (seleccionado == 3)
+            {
+                int selec_alumno;
+                selec_alumno = albums[seleccionada].Canciones.FindIndex(bus => bus.Artista_canción == TextBox_codigo.Text);
+                albums[seleccionada].Canciones.RemoveAt(selec_alumno);
+                GuardarJson(); limpiar_campos();
+                TextBox_codigo.Text = ""; bloqueo(); desbloqueo1(true); Button1.Visible = true;
+            }
         }
     }
 }
