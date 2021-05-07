cdc14fe4503c2c41a89de0c2f15907c6014b1921
Daniel Pérez Chan <danielpc@mesoamericana.edu.gt>
2021-05-07 09:28:54 -0600

    Se termino de programar la primera pestaña

--- Segundo - Examen - Parcial/Clases/Albúm.cs
+++ Segundo - Examen - Parcial/Clases/Albúm.cs
@@ -8,12 +8,14 @@ namespace Segundo___Examen___Parcial.Clases
     {
         string titulo_albúm;
         string artista;
-        string fecha_publicacón;
+        DateTime fecha_publicacón;
         List<Canción> canciones = new List<Canción>();
         public string Titulo_albúm { get => titulo_albúm; set => titulo_albúm = value; }
         public string Artista { get => artista; set => artista = value; }
-        public string Fecha_publicacón { get => fecha_publicacón; set => fecha_publicacón = value; }
+
+        public DateTime Fecha_publicacón { get => fecha_publicacón; set => fecha_publicacón = value; }
         public List<Canción> Canciones { get => canciones; set => canciones = value; }
+       
 
         public Albúm()
         {
--- Segundo - Examen - Parcial/Default.aspx.cs
+++ Segundo - Examen - Parcial/Default.aspx.cs
@@ -2,6 +2,7 @@
 using Segundo___Examen___Parcial.Clases;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -16,8 +17,27 @@ namespace Segundo___Examen___Parcial
         List<Canción> canciones = new List<Canción>();
         protected void Page_Load(object sender, EventArgs e)
         {
+            MaintainScrollPositionOnPostBack = true;
+            /*if (!IsPostBack)
+            {
+                //Cada vez que se corre el programa, se cargan las universidades existentes en el archivo Json
+
+                //se usará el archivo de universidades
+                string archivo = Server.MapPath("Archivo Albumes.json");
+                //se abre el archivo
+                StreamReader jsonStream = File.OpenText(archivo);
+
+                //se lee todo el contenido del archivo y el contenido se guarda en la variable json
+                string json = jsonStream.ReadToEnd();
+
+                jsonStream.Close();
+
+                //Se deserializa (convierte) la cadena json en la estructura que tiene la lista universidades
+                albums = JsonConvert.DeserializeObject<List<Albúm>>(json);
+                bloqueo();
+            }*/
             bloqueo();
-            Button2.Visible = false;
+            Button2.Visible = false; 
         }
         public void bloqueo()
         {
@@ -26,7 +46,7 @@ namespace Segundo___Examen___Parcial
             TextBoxartista.Enabled = false; TextBoxartista.Visible = false;
             Label4.Visible = false;
             Label5.Visible = false; TextBoxtiempo.Enabled = false;
-            TextBoxtiempo.Visible = false;Label6.Visible = false; 
+            TextBoxtiempo.Visible = false;Label6.Visible = false; Button1.Visible = false;
         }
         public void desbloqueo()
         {
@@ -35,7 +55,7 @@ namespace Segundo___Examen___Parcial
             TextBoxartista.Enabled = true; TextBoxartista.Visible = true;
             Label4.Visible = true;
             Label5.Visible = true; TextBoxtiempo.Enabled = true ;
-            TextBoxtiempo.Visible = true; Label6.Visible = true;
+            TextBoxtiempo.Visible = true; Label6.Visible = true; Button1.Visible = true;
         }
         public void limpiar_campos()
         {
@@ -146,7 +166,64 @@ namespace Segundo___Examen___Parcial
         {
 
            Albúm uni = albums.Find(u => u.Titulo_albúm == TextBoxtitulo.Text);
+            if (seleccionado == 0)
+            {
+                if (uni == null)
+                {
+                    Albúm albúm = new Albúm();
+                    albúm.Titulo_albúm= TextBoxtitulo.Text;
+                    albúm.Artista = TextBoxnombre_artista.Text;
+                    albúm.Fecha_publicacón = Calendarfecha_publicación.SelectedDate;
+                    albúm.Canciones = canciones.ToList();
+                    albums.Add(albúm);
+                    GuardarJson();
+                    canciones.Clear();
+                }
+                else
+                {
+                    uni.Canciones = canciones;
+                    GuardarJson();
+                }
 
+            }
+            else if (seleccionado == 1)
+            {
+                if (uni == null)
+                {
+                    Albúm albúm = new Albúm();
+                    albúm.Titulo_albúm = TextBoxtitulo.Text;
+                    albúm.Artista = TextBoxnombre_artista.Text;
+                    albúm.Fecha_publicacón = Calendarfecha_publicación.SelectedDate;
+                    albúm.Canciones = canciones.ToList();
+                    albums.Add(albúm);
+                    GuardarJson();
+                    canciones.Clear();
+                }
+                else
+                {
+                    uni.Canciones = canciones;
+                    GuardarJson();
+                }
+            }
+            else if (seleccionado == 2)
+            {
+                if (uni == null)
+                {
+                    Albúm albúm = new Albúm();
+                    albúm.Titulo_albúm = TextBoxtitulo.Text;
+                    albúm.Artista = TextBoxnombre_artista.Text;
+                    albúm.Fecha_publicacón = Calendarfecha_publicación.SelectedDate;
+                    albúm.Canciones = canciones.ToList();
+                    albums.Add(albúm);
+                    GuardarJson();
+                    canciones.Clear();
+                }
+                else
+                {
+                    uni.Canciones = canciones;
+                    GuardarJson();
+                }
+            }
             Button2.Visible = false;
         }
     }
